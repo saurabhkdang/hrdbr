3bfb218787c1127ea539183ebda549118e32f8d0
Saurabh Dang <saurabh.dang@technoscore.net>
2023-01-17 12:24:57 +0530

    added mettis menu for left nav

--- src/routes/leftside/leftside.component.jsx
+++ src/routes/leftside/leftside.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MetisMenu from '@metismenu/react';
+import './leftside.styles.css';
 
 const LeftNav = () => {
   return (
@@ -10,23 +12,22 @@ const LeftNav = () => {
         </button>
         <div className="sidebar-scroll">
             <nav id="left-sidebar-nav" className="sidebar-nav">
-                <ul id="main-menu" className="metismenu">
-                    <li className="menu_dashboard"><Link to="/"><i className="lnr lnr-home"></i> <span>Dashboard</span></Link></li>
-                    <li className="menu_listuser"><Link to="/"><i className="lnr lnr-users"></i> <span>Employees Details</span></Link></li>
-                    <li className="@yield('menu_performance') @yield('menu_training') @yield('menu_attendance') @yield('menu_job') @yield('menu_holiday')">
-                        <a href="#uiElements" className="has-arrow" aria-expanded="false"><i className="fa fa-building-o"></i> <span>Company</span></a>
+                <MetisMenu>
+                    <li><Link to="/" exact><i className="lnr lnr-home"></i>Dashboard</Link></li>
+                    <li><Link to="/" exact><i className="lnr lnr-users"></i>Employee Details</Link></li>
+                    <li><Link to="#" className="has-arrow"><i className="fa fa-building-o"></i>Company</Link>
                         <ul>
-                            <li className="menu_performance"><Link to="/performance"><i className="lnr lnr-star"></i> <span>Monthly Rating</span></Link></li>
-                            <li className="send_performance"><Link to="/"><i className="fa fa-file-word-o"></i> <span>Send Monthly Rating</span></Link></li>
-                            <li className="menu_training"><Link to="/"><i className="fa fa-calendar"></i> <span>Events</span></Link></li>
-                            <li className="menu_attendance"><Link to="/"><i className="lnr lnr-hourglass"></i> <span>Attendance</span></Link></li>
-                            <li className="menu_job"><Link to="/job"><i className="fa fa-file-word-o"></i> <span>Job Description</span></Link></li>
-                            <li className="menu_holiday"><Link to="/holiday"><i className="fa fa-file-word-o"></i> <span>Holidays</span></Link></li>
+                            <li><Link to="/performance"><i className="lnr lnr-star"></i> <span>Monthly Rating</span></Link></li>
+                            <li><Link to="/"><i className="fa fa-file-word-o"></i> <span>Send Monthly Rating</span></Link></li>
+                            <li><Link to="/"><i className="fa fa-calendar"></i> <span>Events</span></Link></li>
+                            <li><Link to="/"><i className="lnr lnr-hourglass"></i> <span>Attendance</span></Link></li>
+                            <li><Link to="/job"><i className="fa fa-file-word-o"></i> <span>Job Description</span></Link></li>
+                            <li><Link to="/holiday"><i className="fa fa-file-word-o"></i> <span>Holidays</span></Link></li>
                         </ul>
-                    </li>                    
-                    <li className="menu_assign_roles"><Link to="/assign-roles"><i className="lnr lnr-users"></i> <span>Assign Roles</span></Link></li>
+                    </li>
+                    <li><Link to="/assign-roles" exact><i className="lnr lnr-users"></i>Assign Roles</Link></li>
                     <li><a href="mailto:support@interlynxsystems.com"><i className="lnr lnr-question-circle"></i> <span>Support</span></a></li>
-                </ul>
+                </MetisMenu>
             </nav>
         </div>
     </div>
